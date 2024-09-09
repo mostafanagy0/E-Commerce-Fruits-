70a5231625855a0cce1be914d1a65df8e40a4874
mostafanagy0 <mostafanagy855@gmail.com>
2024-09-08 17:18:19 -0700

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_app/core/helper_funcitons/on_generate_routes.dart';
 import 'package:fruits_app/core/services/shered_ferfernces_singleton.dart';
 import 'package:fruits_app/core/utils/app_colors.dart';
 import 'package:fruits_app/features/Splash/presentation/Views/splash_view.dart';
+import 'package:fruits_app/firebase_options.dart';
 import 'package:fruits_app/generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Prefs.init();
   runApp(const MyApp());
 }
